89a5b71e2f4e6d146c36010184cdc6d0d99faa74
seansummer <cloudninth@163.com>
2013-07-19 13:19:10 +0800

    logging create

--- MApi.py
+++ MApi.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import hashlib, os, sys, tempfile, time, ConfigParser
+import hashlib, os, sys, tempfile, time, ConfigParser, logging
 from ctp import ApiStruct, MdApi, TraderApi   
         
 class MyMdApi(MdApi):
+    FORMAT= '%(name)s %(levelname)s %(asctime)s %(process)d %(processName)s %(thread)d %(threadName)s %(message)s'
+    logging.basicConfig(filename='./log/' + time.strftime('%Y-%m-%d',time.localtime(time.time())) + 'mdlog.log', filemode='a', level=logging.DEBUG, format=FORMAT)
 #初始化MyMdApi类
     def __init__(self, brokerID, userID, password, instrumentIDs):
         self.requestID = 0
@@ -32,7 +34,7 @@ class MyMdApi(MdApi):
 
 #当客户端与交易后台建立起通信连接时（还未登录前），该方法被调用。
     def OnFrontConnected(self):
-        print('OnFrontConnected: Connect...')
+        logging.info('OnFrontConnected: Connect...')
         req = ApiStruct.ReqUserLogin(BrokerID=self.brokerID, UserID=self.userID, Password=self.password)
         self.requestID += 1
         self.ReqUserLogin(req, self.requestID)
@@ -45,44 +47,45 @@ class MyMdApi(MdApi):
 #        0x2002 发送心跳失败
 #        0x2003 收到错误报文
     def OnFrontDisconnected(self, nReason):
-        print('OnFrontDisconnected:', nReason)
+        logging.info('OnFrontDisconnected:' + str(nReason))
 
 #心跳超时警告。当长时间未收到报文时，该方法被调用。
 #@param nTimeLapse 距离上次接收报文的时间
     def OnHeartBeatWarning(self, nTimeLapse):
-        print('OnHeartBeatWarning:', nTimeLapse)
+        logging.info('OnHeartBeatWarning:' + str(nTimeLapse))
 
 #登录请求响应
     def OnRspUserLogin(self, pRspUserLogin, pRspInfo, nRequestID, bIsLast):
-        print('OnRspUserLogin:', pRspInfo)
+        logging.info('OnRspUserLogin:' + str(pRspInfo))
         if pRspInfo.ErrorID == 0: # Success
-            print('GetTradingDay:', self.GetTradingDay())
+            logging.info('GetTradingDay:' + self.GetTradingDay())
             self.SubscribeMarketData(self.instrumentIDs)
 
 #订阅行情应答
     def OnRspSubMarketData(self, pSpecificInstrument, pRspInfo, nRequestID, bIsLast):
-        print('OnRspSubMarketData:', pRspInfo)
+        logging.info('OnRspSubMarketData:' + str(pRspInfo))
 
 #退订行情应答
     def OnRspUnSubMarketData(self, pSpecificInstrument, pRspInfo, nRequestID, bIsLast):
-        print('OnRspUnSubMarketData:', pRspInfo)
+        logging.info('OnRspUnSubMarketData:' + str(pRspInfo))
 
 #错误应答
     def OnRspError(self, pRspInfo, nRequestID, bIsLast):
-        print('OnRspError:', pRspInfo)
+        logging.error('OnRspError:'+ str(pRspInfo))
 
 #登出请求应答
     def OnRspUserLogout(self, pUserLogout, pRspInfo, nRequestID, bIsLast):
-        print('OnRspUserLogout:', pRspInfo)
+        logging.info('OnRspUserLogout:'+ str(pRspInfo))
 
 #深度行情通知
     def OnRtnDepthMarketData(self, pDepthMarketData):
         data = pDepthMarketData
         #print('OnRspQryDepthMarketData:')
-        print("%s|%.2f|%.2f|%.2f|%d|%s|%.2f|%d|%.2f|%d" % (data.InstrumentID,data.LastPrice,data.HighestPrice,data.LowestPrice,data.Volume,data.UpdateTime,data.BidPrice1,data.BidVolume1,data.AskPrice1,data.AskVolume1))
+        datas = "{0:s}|{1:.2f}|{2:.2f}|{3:.2f}|{4:d}|{5:s}|{6:.2f}|{7:d}|{8:.2f}|{9:d}".format(data.InstrumentID,data.LastPrice,data.HighestPrice,data.LowestPrice,data.Volume,data.UpdateTime,data.BidPrice1,data.BidVolume1,data.AskPrice1,data.AskVolume1)
         #print("合约:%s\n最新价:%.2f\t最高价:%.2f\t最低价:%.2f\t数量:%d\t最后修改时间:%s\n买价:%.2f\t买量:%d\t卖价:%.2f\t卖量:%d" % (d.InstrumentID,d.LastPrice,d.HighestPrice,d.LowestPrice,d.Volume,d.UpdateTime,d.BidPrice1,d.BidVolume1,d.AskPrice1,d.AskVolume1))
-        f = open('mdlog.txt','a')
-        f.write(str(data)+'\n')
+        mdname = './log/' + time.strftime('%Y-%m-%d',time.localtime(time.time())) + 'md.txt'
+        f = open(mdname,'a')
+        f.write(str(datas)+'\n')
         f.close()
         
 def mdconnect():
